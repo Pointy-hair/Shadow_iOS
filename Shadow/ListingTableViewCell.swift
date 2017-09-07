aea4eb3222f95c9f9cc8d2b09362b085316be407
Atinderjit Kaur <atinderjit@Atinderjit-Kaur-iPhone.local>
2017-09-07 10:20:36 +0530

    today change

---
+++ Shadow/ListingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListingTableViewCell.swift
+//  Shadow
+//
+//  Created by Atinderjit Kaur on 06/09/17.
+//  Copyright © 2017 Atinderjit Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ListingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Shadow/ListingViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ListingViewController.swift
+//  Shadow
+//
+//  Created by Atinderjit Kaur on 06/09/17.
+//  Copyright © 2017 Atinderjit Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ListingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /**
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ListingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    return 10
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let cell = tableView.dequeueReusableCell(withIdentifier: "cell_listing", for: indexPath) as! ListingTableViewCell
+        return cell
+     
+    }
+    
+    
+}
