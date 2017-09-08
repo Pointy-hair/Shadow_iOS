c638acd6dd7aaacfdfa7653533b700db1d082c7d
Aditi <aditi@Aditi-CreativeCrew.local>
2017-09-08 12:43:37 +0530

    Changes 06/9

--- Shadow/Shadow/SendRequestViewController.swift
+++ Shadow/Shadow/SendRequestViewController.swift
@@ -25,7 +25,7 @@ class SendRequestViewController: UIViewController,GMSAutocompleteViewControllerD
     @IBOutlet var scrollView: UIScrollView!
     
     var user_Name:String?
-    
+    var user_location:String?
     var request_id_fromRequestDetail:NSNumber?
     var check_comingFromRequestDetail:String = "NO"
     
@@ -100,6 +100,12 @@ class SendRequestViewController: UIViewController,GMSAutocompleteViewControllerD
         self.imgView_Virtually.image = UIImage.init(named: "unchecked")
         
         self.btn_SelectLocation.isHidden = false
+        if user_location != nil {
+        self.btn_SelectLocation.setTitle(self.user_location!, for: .normal)
+        }else{
+            self.btn_SelectLocation.setTitle("Select Location", for: .normal)
+ 
+        }
         //  self.btn_SelectVirtualOption.isHidden = true
         
         
@@ -151,7 +157,6 @@ class SendRequestViewController: UIViewController,GMSAutocompleteViewControllerD
                     self.showAlert(Message: "Please select a date.", vc: self)
                 }
             }
-                
             else if self.imgView_Virtually.image == UIImage.init(named: "checked"){
                 if str_date_selected != ""{
                     if txtView_Message.text != ""  {
